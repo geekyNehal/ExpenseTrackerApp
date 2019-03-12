05d6e946aaf653c2fc59a1c6340f5edf1badabce
Nehal Kumar Singh <nehal@localhost.localdomain>
2019-03-12 20:32:07 +0530

    Firebase json file added

--- app/src/main/java/com/project/geekynehal/expensetrackerapp/MainActivity.java
+++ app/src/main/java/com/project/geekynehal/expensetrackerapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.project.geekynehal.expensetrackerapp;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity
+{
+    private EditText login_username,login_password;
+    private Button signIn;
+    private TextView signUp;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mDialog;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
