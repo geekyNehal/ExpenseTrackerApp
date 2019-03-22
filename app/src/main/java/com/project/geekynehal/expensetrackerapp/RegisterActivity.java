6d93762070da6667fe68eea66a150ea46d5aa1f4
Nehal Kumar Singh <nehal@localhost.localdomain>
2019-03-23 00:17:48 +0530

    Login OnClickListener in RegisterActivity Added

--- app/src/main/java/com/project/geekynehal/expensetrackerapp/RegisterActivity.java
+++ app/src/main/java/com/project/geekynehal/expensetrackerapp/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.project.geekynehal.expensetrackerapp;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -25,5 +27,18 @@ public class RegisterActivity extends AppCompatActivity {
         btnReg=findViewById(R.id.sign_up_btn);
         signin=findViewById(R.id.sign_in_btn);
         mAuth=FirebaseAuth.getInstance();
+        mDialog=new ProgressDialog(this);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
