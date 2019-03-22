be725ca9589cda47742e02375a8d385ec78713fa
Nehal Kumar Singh <nehal@localhost.localdomain>
2019-03-22 23:23:32 +0530

    Login Activity work completed

--- app/src/main/java/com/project/geekynehal/expensetrackerapp/MainActivity.java
+++ app/src/main/java/com/project/geekynehal/expensetrackerapp/MainActivity.java
@@ -1,12 +1,20 @@
 package com.project.geekynehal.expensetrackerapp;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity
@@ -22,6 +30,51 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        login_username=findViewById(R.id.login_user);
+        login_password=findViewById(R.id.login_pass);
+        signIn=findViewById(R.id.sign_in_btn);
+        signUp=findViewById(R.id.sign_up_text);
+        mAuth=FirebaseAuth.getInstance();
+        mDialog=new ProgressDialog(this);
+        if(mAuth.getCurrentUser()!=null)
+        {
+            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+        }
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+            }
+        });
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    String mEmail=login_username.getText().toString().trim();
+                    String mPass=login_password.getText().toString().trim();
+                    if(TextUtils.isEmpty(mEmail))
+                    {
+                        login_username.setError("Required Field");
+                        return ;
+                    }
+                    if(TextUtils.isEmpty(mPass))
+                    {
+                        login_password.setError("Required Field");
+                        return ;
+                    }
+                    mDialog.setMessage("Please Wait while Processing..");
+                    mDialog.show();
+                    mAuth.signInWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                                mDialog.dismiss();
+                            }
+                        }
+                    });
+            }
+        });
     }
 }
